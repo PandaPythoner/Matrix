a1257b9aebb76c7d965be23eb4e08c877dc5b0cb
pandapythoner <stepanov.aa@students.sch2.ru>
2021-09-29 10:46:53 +0300

    Added simple interface to main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,27 @@ using namespace std;
 
 int main() {
     try {
-        matrix<long long> a({{1, 2},
-                             {3, 4}});
-        matrix<long long> b({{4, 3},
-                             {2, 1}});
-        cin >> a >> b;
-        int n = a.shape().first;
-        int m = a.shape().second;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                cout << (a + b)[i][j] << " ";
-            }
-            cout << "\n";
+        matrix<long long> a, b;
+        long long x, y;
+        cout << "Type first matrix (a):\n";
+        cin >> a;
+        cout << "Type second matrix (b):\n";
+        cin >> b;
+        cout << "Type number for multiplication (x):";
+        cin >> x;
+        cout << "Type number for pow (y):";
+        cin >> y;
+        if(a.shape() == b.shape()){
+            cout << "a + b = \n" << a + b << "\n\n";
+        }
+        cout << "a * x = \n" << a * x << "\n\n";
+        if(a.shape().second == b.shape().first) {
+            cout << "a * b = \n";
+            cout << a * b << "\n\n";
+        }
+        cout << "transpose(a) = \n" << transpose(a) << "\n\n";
+        if(a.shape().first == a.shape().second){
+            cout << "a ** y = \n" << pow(a, y) << "\n\n";
         }
     }
     catch(char const* s){
